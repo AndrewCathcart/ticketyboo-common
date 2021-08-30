8991673f0c4eb3db2be6adf20bcd14fd47878169
Andrew Cathcart <newcastleandrewc@gmail.com>
2021-08-30 16:22:18 +0100

    Updates

--- dist/events/base-listener.d.ts
+++ dist/events/base-listener.d.ts
@@ -1,5 +1,5 @@
 import { Message, Stan } from 'node-nats-streaming';
-import { Subjects } from './subjects';
+import { Subjects } from './models/subjects';
 interface Event {
     subject: Subjects;
     data: any;
